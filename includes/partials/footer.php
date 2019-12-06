90b4ae551e09d0d00ac6bce2037a8083d5becd17
Jesper <jespersundin8@gmail.com>
2019-12-06 01:24:20 +0100

    Setup

---
+++ includes/partials/footer.php
@@ -0,0 +1,5 @@
+</body>
+<footer class="footer">
+    <script src="jquery-3.4.1.min.js"></script>
+</footer>
+</html>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    require_once ".\includes\partials/header.php";   
+?>
+
+<h1> Title </h1>
+
+
+<?php
+    require_once ".\includes\partials/footer.php";   
+?>
