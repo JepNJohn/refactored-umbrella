ea463b566c108f7ec47ce8e5a6a5d0ba15c4837c
Jesper <jespersundin8@gmail.com>
2019-12-10 17:36:38 +0100

    index update

--- index.php
+++ index.php
@@ -2,8 +2,50 @@
     require_once ".\includes\partials/header.php";   
 ?>
 
-<h1> Title </h1>
+    <div class="topImg">
+        <div class="ptext">
+            <span class="border">
+            Parallax Website
+            </span>
+        </div>
+    </div>
 
+    <section class="section section-light">
+        <h2>section One</h2>
+        <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque voluptates tempore, temporibus voluptatum cum animi dolores a rem tempora ipsam eum at laborum error molestiae consequatur nam distinctio porro fuga eos provident odit ea incidunt qui quam. Ipsum, nisi iusto assumenda sapiente doloribus vel quo quidem porro id ex accusantium delectus reiciendis voluptas, itaque, laboriosam atque quam impedit! Dolore deserunt sed aliquam nobis, magni doloremque nam alias ea. Ea soluta iusto, modi, architecto nemo in aliquid delectus rerum voluptas pariatur sunt a vel accusamus nesciunt itaque nam doloribus velit mollitia! Animi odio blanditiis, eius error consequuntur sequi quod obcaecati quia!
+        </p>
+    </section>
+
+    <div class="midImg">
+        <div class="ptext">
+            <span class="border">
+            Image two
+            </span>
+        </div>
+    </div>
+
+    <section class="section section-dark">
+        <h2>section two</h2>
+        <p>
+        Lorem ipsum dolo isicing elit. Cumque voluptates tempore, temporibus voluptatum cum animi dolores a rem tempora ipsam eum at laborum error molestiae consequatur nam distinctio porro fuga eos provident odit ea incidunt qui quam. Ipsum, nisi iusto assumenda sapiente doloribus vel quo quidem porro id ex accusantium delectus reiciendis voluptas, itaque, laboriosam atque quam impedit! Dolore deserunt sed aliquam nobis, magni doloremque nam alias ea. Ea soluta iusto, modi, architecto nemo in aliquid delectus rerum voluptas pariatur sunt a vel accusamus nesciunt itaque nam doloribus velit mollitia! Animi odio blanditiis, eius error consequuntur sequi quod obcaecati quia!
+        </p>
+        
+    <div class="botImg">
+        <div class="ptext">
+            <span class="border">
+            Image three
+            </span>
+        </div>
+    </div>
+    </section>
+
+    <section class="section section-dark">
+        <h2>section Three</h2>
+        <p>
+        Lorem ipsum dolo que voluptates tempore, temporibus voluptatum cum animi dolores a rem tempora ipsam eum at laborum error molestiae consequatur nam distinctio porro fuga eos provident odit ea incidunt qui quam. Ipsum, nisi iusto assumenda sapiente doloribus vel quo quidem porro id ex accusantium delectus reiciendis voluptas, itaque, laboriosam atque quam impedit! Dolore deserunt sed aliquam nobis, magni doloremque nam alias ea. Ea soluta iusto, modi, architecto nemo in aliquid delectus rerum voluptas pariatur sunt a vel accusamus nesciunt itaque nam doloribus velit mollitia! Animi odio blanditiis, eius error consequuntur sequi quod obcaecati quia!
+        </p>
+    </section>
 
 <?php
     require_once ".\includes\partials/footer.php";   
